66be63dd43865ed6e7d4199ecb76fd508401c951
Paige Olsen <paige.el.olsen@gmail.com>
2022-11-03 09:45:19 -0700

    added functionality for adding and removing tasks and toggling form with button

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonVue 
+      :text="showAddTask ? 'Close':'Add Task'" 
+      :color="showAddTask ? 'red':'green'"
+      @btn-click="$emit('toggle-add-task')" 
+    /> 
+  </header>
+</template>
+
+<script>
+  import ButtonVue from './Button.vue';
+
+  export default {
+    name: 'HeaderVue',
+    props: {
+      title: {
+        type: String,
+        default: 'Hello World'
+      },
+      showAddTask: Boolean
+    },
+    components: {
+      ButtonVue,
+    },
+    emits: ['toggle-add-task']
+  }
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
